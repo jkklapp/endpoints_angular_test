c3101c2cd75065274c204ec64754ab5e9cf2ff1f
Jaakko Lappalainen <jkk.lapp@gmail.com>
2015-10-26 15:01:41 +0000

    Modified UI for the Authed POST method.

--- static/js/app.js
+++ static/js/app.js
@@ -8,16 +8,34 @@ app.controller('GreetingsController', ['$http', function($http) {
 
 	var that = this;
 
+	//this has to be acquired from an auth API endpoint
+	this.token = 'secret';
+
 	$http.get('/_ah/api/helloworld/v1/hellogreeting').success(function(data) {
 		that.greetings = data["items"];
 	});
 
-	this.addGreeting = function() {
-		//this.greetings.push(this.greetingText);
-		$http.post('/_ah/api/helloworld/v1/hellogreeting/authed', {'message': this.message}).success(function(data) {
-			that.greetings.push(data);
-			that.message = "";
-		});
+	this.addAnonGreeting = function() {
+		$http({
+			method: "POST",
+			url: '/_ah/api/helloworld/v1/hellogreeting/authed',
+		 	data: {'message': this.message}
+		}).success(that.callback);
+	};
+
+	this.addAuthedGreeting = function() {
+		var token = this.token;
+	  $http({
+			method: "POST",
+			url: '/_ah/api/helloworld/v1/hellogreeting/authed?access_token='+token,
+			data: {'message': this.message},
+			headers: {'Authorization': 'Bearer '+token}
+		}).success(that.callback);
+	};
+
+	this.callback = function(data) {
+		that.greetings.push(data);
+		that.message = "";
 	};
 
 }]);
