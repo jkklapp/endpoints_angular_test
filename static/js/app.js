27cbfbd11cf5e3818d33340d7e95d06b477c7f6f
Jaakko Lappalainen <jkk.lapp@gmail.com>
2015-10-27 08:12:33 +0100

    Modularize a bit more JWT generation. Clean-up the code.

--- static/js/app.js
+++ static/js/app.js
@@ -7,24 +7,23 @@ app.controller("GreetingsController", ["$http", function($http) {
 	this.message = "";
 	this.author = "";
 
-	/*var jwt_string = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0N"+
-					 "TY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWV9.TJVA" +
-					 "95OrM7E2cBab30RMHrHDcEfxjoYZgeFONFh7HgQ";*/
-
-	var jwt_header = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9";
-
 	var that = this;
 
 	$http.get("/_ah/api/helloworld/v1/hellogreeting").success(function(data) {
 		that.greetings = data["items"];
 	});
 
-	this.addAuthedGreeting = function() {
-		var token = this.token;
-		var encoded_message = btoa(JSON.stringify({"message": this.message, "name": this.author}))
+	this.generateJwtString = function(message, author) {
+		var jwt_header = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9";
+		var encoded_message = btoa(JSON.stringify({"message": message, "name": author}))
 		var hash = CryptoJS.HmacSHA256(jwt_header + '.' + encoded_message, "secret");
   		var hashInBase64 = CryptoJS.enc.Base64.stringify(hash);
   		var jwt_string = jwt_header + '.' + encoded_message + '.' + hashInBase64;
+  		return jwt_string;
+	};
+
+	this.addAuthedGreeting = function() {
+		var jwt_string = this.generateJwtString(this.message, this.author);
 		$http({
 			method: "POST",
 			url: "/_ah/api/helloworld/v1/hellogreeting/authed",
